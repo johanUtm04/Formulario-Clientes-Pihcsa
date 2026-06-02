9c071c9d1d94c908eff179e9f07ddf0c09098282
johanUtm04 <johanlopezrey1@gmail.com>
2026-06-02 01:21:54 -0600

    add currect data structure

--- formulario.sql
+++ formulario.sql
@@ -24,26 +24,29 @@ DROP TABLE IF EXISTS `formulario_clientes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `formulario_clientes` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `razon_social` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `domicilio` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `poblacion` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `colonia` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `cp` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `estado` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `rfc` varchar(13) COLLATE utf8mb4_general_ci NOT NULL,
-  `pagina_web` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `telefono` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `email` varchar(150) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_licencia_sanitaria` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_aviso_responsableSanitario` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_aviso_funcionamiento` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_ine_responsableSanitario` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_ine_representanteLegal` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `doc_comprobante_domicilio` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `razon_social` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `domicilio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `poblacion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `colonia` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cp` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `estado` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `rfc` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pagina_web` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `firma_digital` text COLLATE utf8mb4_general_ci,
+  `doc_licencia_sanitaria` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_aviso_responsableSanitario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_aviso_funcionamiento` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_ine_responsableSanitario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_ine_representanteLegal` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_comprobante_domicilio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img_fachada` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img_almacen` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `rfc` (`rfc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,6 +55,7 @@ CREATE TABLE `formulario_clientes` (
 
 LOCK TABLES `formulario_clientes` WRITE;
 /*!40000 ALTER TABLE `formulario_clientes` DISABLE KEYS */;
+INSERT INTO `formulario_clientes` VALUES (1,'MEDICADEPOT','mayapan','morelia','mision del valle','58304','Michoacán','LORJ040314FH8','','3344222112','johanlopezrey1@gmail.com','Johan Jael López Reyese','doc_licencia_sanitaria.pdf',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2026-06-02 06:29:36');
 /*!40000 ALTER TABLE `formulario_clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,13 +68,13 @@ DROP TABLE IF EXISTS `usuarios_admin`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios_admin` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,7 +83,7 @@ CREATE TABLE `usuarios_admin` (
 
 LOCK TABLES `usuarios_admin` WRITE;
 /*!40000 ALTER TABLE `usuarios_admin` DISABLE KEYS */;
-INSERT INTO `usuarios_admin` VALUES (1,'admin','$2y$10$mC3Bv8AghQfK.p8G3G9p1ex8A29gC7qLgVvWvE9z3zXhN2yM3K6U.','Administrador PIHCSA','2026-06-01 23:03:25'),(2,'johan','$2y$10$oR1T6oT4802M9T79ZtYfLuYmB.7ZJ72X3r8FclC2q6D7rJjC2tZmq','johan','2026-06-01 23:20:55');
+INSERT INTO `usuarios_admin` VALUES (1,'admin','$2y$10$mC3Bv8AghQfK.p8G3G9p1ex8A29gC7qLgVvWvE9z3zXhN2yM3K6U.','Administrador PIHCSA','2026-06-01 23:03:25'),(2,'johan','$2y$10$oR1T6oT4802M9T79ZtYfLuYmB.7ZJ72X3r8FclC2q6D7rJjC2tZmq','johan','2026-06-01 23:20:55'),(3,'johan2004','morelia123','johan ','2026-06-02 02:45:19'),(4,'nuevo_usuario','$2y$10$NCKk.Rv8WF1O0C9uVJzon.AUVujpS58aNF0oKNOxbXIb2ts8upnd2','Nuevo admin','2026-06-02 06:12:50');
 /*!40000 ALTER TABLE `usuarios_admin` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -92,4 +96,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-01 17:29:55
+-- Dump completed on 2026-06-02  1:21:22

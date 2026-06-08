1a30564023e4b72660e406dbae15f5c9c1e25996
Johan Lopez <johan@corporacionazul.com>
2026-06-08 16:27:31 -0600

    feat: database performance schema

---
+++ formularioClientesProveedoresNuevo.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 8.4.3, for Win64 (x86_64)
+--
+-- Host: localhost    Database: formulario
+-- ------------------------------------------------------
+-- Server version	8.4.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `formulario_clientes`
+--
+
+DROP TABLE IF EXISTS `formulario_clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `formulario_clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `razon_social` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `domicilio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `poblacion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `colonia` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cp` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `estado` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `rfc` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pagina_web` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `firma_digital` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `doc_licencia_sanitaria` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_aviso_responsableSanitario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_aviso_funcionamiento` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_ine_responsableSanitario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_ine_representanteLegal` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `doc_comprobante_domicilio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img_fachada` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img_almacen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rfc` (`rfc`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formulario_clientes`
+--
+
+LOCK TABLES `formulario_clientes` WRITE;
+/*!40000 ALTER TABLE `formulario_clientes` DISABLE KEYS */;
+INSERT INTO `formulario_clientes` VALUES (1,'MEDICADEPOT','mayapan','morelia','mision del valle','58304','Michoacán','LORJ040314FH8','','3344222112','johanlopezrey1@gmail.com','Johan Jael López Reyese','doc_licencia_sanitaria.pdf',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2026-06-02 06:29:36'),(2,'CINE LA SOLEDAD','mayapan','Morelia','La soledad','58304','Michoacán','LOCLLOPPPIDIS','','4435817229','cinesoledad@gmail.com','Johan Jael López Reyes','doc_licencia_sanitaria.pdf',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2026-06-02 17:53:34'),(3,'Médica Integral del Centro S.A. de C.V.','Av. Acueducto #1040, Int. 4B','Morelia','Chapultepec Norte','58260','Michoacán','MED980512TR4','https://www.medicadelcentro.com.mx','4433142055','contacto@medicadelcentro.com.mx','Dr. Alejandro Mendoza Ruiz','doc_licencia_sanitaria.pdf','doc_aviso_responsableSanitario.pdf','doc_aviso_funcionamiento.pdf','doc_ine_responsableSanitario.pdf','doc_ine_representanteLegal.pdf','doc_comprobante_domicilio.pdf','img_fachada.png','img_almacen.png','2026-06-03 20:25:08');
+/*!40000 ALTER TABLE `formulario_clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios_admin`
+--
+
+DROP TABLE IF EXISTS `usuarios_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios_admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios_admin`
+--
+
+LOCK TABLES `usuarios_admin` WRITE;
+/*!40000 ALTER TABLE `usuarios_admin` DISABLE KEYS */;
+INSERT INTO `usuarios_admin` VALUES (1,'admin','$2y$10$mC3Bv8AghQfK.p8G3G9p1ex8A29gC7qLgVvWvE9z3zXhN2yM3K6U.','Administrador PIHCSA','2026-06-01 23:03:25'),(2,'johan','$2y$10$oR1T6oT4802M9T79ZtYfLuYmB.7ZJ72X3r8FclC2q6D7rJjC2tZmq','johan','2026-06-01 23:20:55'),(3,'johan2004','morelia123','johan ','2026-06-02 02:45:19'),(4,'nuevo_usuario','$2y$10$NCKk.Rv8WF1O0C9uVJzon.AUVujpS58aNF0oKNOxbXIb2ts8upnd2','Nuevo admin','2026-06-02 06:12:50'),(5,'jjrayder','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','johan','2026-06-02 17:54:41'),(8,'rayder','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','johan','2026-06-02 17:55:16'),(14,'ren','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Johan Admin','2026-06-02 17:57:34');
+/*!40000 ALTER TABLE `usuarios_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-08 16:27:12
